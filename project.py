c1d44c6894465182b53947f9d30cbe10f5f926de
Jonathan Broderick <jonathanbroderick007@gmail.com>
2017-06-23 07:26:21 -0700

    Add basic templating

--- project.py
+++ project.py
@@ -1,27 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, jsonify, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Animal, Toy
+
 app = Flask(__name__)
 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from database_setup import Base, Animal, Toy
-#
-# engine = create_engine('sqlite:///animal_toys.db')
-# Base.metadata.bind = engine
-#
-# DBSession = sessionmaker(bind=engine)
-# session = DBSession
+engine = create_engine('sqlite:///animal_toys.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession
+
 
 @app.route('/')
 @app.route('/animals')
 def showAnimals():
-    #animals = session.query(Animal).all()
-    return 'This page will list all the animals.'
+    animals = session.query(Animal).all()
+    #return 'This page will list all the animals.'
+    return render_template('animals.html', animals = animals)
 
 
 @app.route('/animal/<int:animal_id>/')
 @app.route('/animal/<int:animal_id>/toys/')
 def showToys(animal_id):
-    return 'This page will list the toys for a particular animal.'
+    animal = session.query(Animal).filter_by(id = animal_id).one()
+    toys = session.query(Toy).filter_by(animal_id = animal_id).all()
+    #return 'This page will list the toys for a particular animal.'
+    return render_template('toys.html', toys = toys, animal = animal)
 
 
 
