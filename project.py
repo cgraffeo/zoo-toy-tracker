0b37a547371eeefe22d24706818843934a720754
Jonathan Broderick <jonathanbroderick007@gmail.com>
2017-06-25 20:09:52 -0700

    Add 'editAnimal' and 'deleteAnimal' routes and links

--- project.py
+++ project.py
@@ -17,27 +17,38 @@ session = DBSession()
 def showAnimals():
     animals = session.query(Animal).all()
     #return 'This page will list all the animals.'
-    return render_template('animals.html', animals = animals)
+    return render_template('animals.html', animals=animals)
 
 
 @app.route('/animal/new', methods=['GET', 'POST'])
 def newAnimal():
-    if request.method == 'POST':
-        newAnimal = Animal(name=request.form['name'])
-        session.add(newAnimal)
-        session.commit()
-        return redirect(url_for('showAnimals'))
-    else:
-        return render_template('newAnimal.html')
+    return 'This page will be for making a new animal'
+    # if request.method == 'POST':
+    #     newAnimal = Animal(name=request.form['name'])
+    #     session.add(newAnimal)
+    #     session.commit()
+    #     return redirect(url_for('showAnimals'))
+    # else:
+    #     return render_template('newAnimal.html')
+
+
+@app.route('/animal/<int:animal_id>/edit', methods=['GET', 'POST'])
+def editAnimal(animal_id):
+    return 'This page will be for editing this animal'
+
+
+@app.route('/animal/<int:animal_id>/delete', methods=['GET', 'POST'])
+def deleteAnimal(animal_id):
+    return 'This page will be for deleting this animal'
 
 
 @app.route('/animal/<int:animal_id>')
 @app.route('/animal/<int:animal_id>/toys')
 def showToys(animal_id):
-    animal = session.query(Animal).filter_by(id = animal_id).one()
-    toys = session.query(Toy).filter_by(animal_id = animal_id).all()
+    animal = session.query(Animal).filter_by(id=animal_id).one()
+    toys = session.query(Toy).filter_by(animal_id=animal_id).all()
     #return 'This page will list the toys for a particular animal.'
-    return render_template('toys.html', toys = toys, animal = animal)
+    return render_template('toys.html', toys=toys, animal=animal)
 
 
 if __name__ == '__main__':
