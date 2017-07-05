d82d7072ee1ed9e078efec0fdb64229619f9f078
jonathanb <jonathanbroderick007@gmail.com>
2017-07-05 15:02:13 -0700

    Swap out photo URLs for better sizes

--- populate_db.py
+++ populate_db.py
@@ -10,8 +10,9 @@ session = DBSession()
 
 # Toys for Riley the giraffe
 giraffe1 = Animal(name='Riley', age=22, species='Giraffe',
-    photo='http://animalinyou.com/images/animals/giraffe/giraffe-2.jpg',
-    user_id=1)
+    photo='''
+    https://www.gannett-cdn.com/-mm-/23a4d1bc400338a4d357213197131161c2b0c9ce/c=119-0-2001-1415&r=x404&c=534x401/local/-/media/2017/03/02/USATODAY/USATODAY/636240538720039138-GettyImages-509934328.jpg
+    ''', user_id=1)
 
 session.add(giraffe1)
 session.commit()
@@ -20,8 +21,8 @@ giraffe1_toy1 = Toy(name='Antlers', toy_type='Sensory',
     description='''
     Antlers from a deer that provide sensory stimulation for the animal.''',
     photo='''
-    http://cdn0.wideopenspaces.com/wp-content/uploads/2014/01/featured-antlers.jpg''',
-    animal=giraffe1, user_id=1)
+    http://img-aws.ehowcdn.com/300x200/cpi.studiod.com/www_ehow_com/photos.demandstudios.com/getty/article/235/128/466280485_XS.jpg
+    ''', animal=giraffe1, user_id=1)
 
 session.add(giraffe1_toy1)
 session.commit()
@@ -31,8 +32,8 @@ giraffe1_toy2 = Toy(name='Barrel Feeder', toy_type='Foods and Feeding',
     Just like it sounds. Food placed in a barrel with enough openings for
     access.''',
     photo='''
-    https://images-na.ssl-images-amazon.com/images/I/41Qj8Bh185L._SY355_.jpg''',
-    animal=giraffe1, user_id=1)
+    https://images-na.ssl-images-amazon.com/images/I/41Qj8Bh185L._SY355_.jpg
+    ''', animal=giraffe1, user_id=1)
 
 session.add(giraffe1_toy2)
 session.commit()
@@ -40,7 +41,7 @@ session.commit()
 giraffe1_toy3 = Toy(name='Pinecones', toy_type='Manipulative',
     description='''
     Pine cones used for an interactive toy for removing parts.''',
-    photo='http://www.timespub.tc/wp-content/uploads/2012/01/Pine-cones-1.jpg',
+    photo='http://www.winterwoods.com/art/cones/cone-sand.gif',
     animal=giraffe1, user_id=1)
 
 session.add(giraffe1_toy3)
@@ -49,10 +50,7 @@ session.commit()
 
 # Toys for Tino the gorilla
 gorilla1 = Animal(name='Tino', age=15, species='Gorilla',
-    photo='''
-    http://animals.sandiegozoo.org/sites/default/files/2016-09/Gorilla_ZN.jpg
-    ''',
-    user_id=1)
+    photo='http://pin.primate.wisc.edu/fs/sheets/images/235med.jpg', user_id=1)
 
 session.add(gorilla1)
 session.commit()
@@ -62,8 +60,8 @@ gorilla1_toy1 = Toy(name='TV/Videos', toy_type='Sensory',
     Pretty obvious what this is. Should only be used for 30 minutes or less
     a day.''',
     photo='''
-    http://sugikingdom.com/wp-content/uploads/2016/12/2017-TV-GUide.jpg''',
-    animal=gorilla1, user_id=1)
+    http://az616578.vo.msecnd.net/files/2016/05/14/635988030950093450571401770_tv_bars.jpg
+    ''', animal=gorilla1, user_id=1)
 
 session.add(gorilla1_toy1)
 session.commit()
@@ -83,9 +81,7 @@ gorilla1_toy3 = Toy(name='Tug-o-war', toy_type='Behavioral/Social',
     description='''
     A very sturdy rope for use between animal/keeper or animal/animal.
     Be careful with this one!''',
-    photo='''
-    https://www.globalbodyweighttraining.com/wp-content/uploads/GBT-Manila-Rope-Knot.jpg
-    ''', animal=gorilla1, user_id=1)
+    photo='https://i.stack.imgur.com/MMQggm.jpg', animal=gorilla1, user_id=1)
 
 session.add(gorilla1_toy3)
 session.commit()
@@ -94,7 +90,7 @@ session.commit()
 # Toys for Zuri the elephant
 elephant1 = Animal(name='Zuri', age=10, species='Elephant',
     photo='''
-    https://static1.squarespace.com/static/5304f39be4b0c1e749b456be/t/58cc06d96a49637cb9ed84a6/1489766903764/
+    https://elephants-media.s3.amazonaws.com/App/Models/Elephant/images/000/000/004/thumbnail/Photo%20Of%20Flora%20At%20The%20Elephant%20Sanctuary%20In%20Tennessee.jpg
     ''', user_id=1)
 
 session.add(elephant1)
@@ -104,9 +100,8 @@ elephant1_toy_1 = Toy(name='Bubbles', toy_type='Sensory',
     description='''
     The keeper can blow NON-TOXIC bubbles into the elephant exhibit.
     The elephants like to chase them around.''',
-    photo='''
-    https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iz9tFMUcP6rI/v1/1200x-1.jpg
-    ''', animal=elephant1, user_id=1)
+    photo='http://3dprint.com/wp-content/uploads/2015/07/bubble4.png',
+    animal=elephant1, user_id=1)
 
 session.add(elephant1_toy_1)
 session.commit()
@@ -115,7 +110,7 @@ session.commit()
 # Toys for Kazek the tiger
 tiger1 = Animal(name='Kazek', age=13, species='Tiger',
     photo='''
-    http://www.wildaid.org/sites/default/files/photos/iStock_000008484745Large%20%20tiger%20-%20bengal.jpg
+    https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Bengal_Tiger.png/220px-Bengal_Tiger.png
     ''', user_id=1)
 
 session.add(tiger1)
@@ -125,9 +120,8 @@ tiger1_toy1 = Toy(name='Disco Ball', toy_type='Sensory',
     description='''
     The little lights off this disco ball are like using a laser on a
     house cat; The tigers love to chase them around.''',
-    photo='''
-    https://images-na.ssl-images-amazon.com/images/I/61ynRSp-T1L._SY355_.jpg
-    ''', animal=tiger1, user_id=1)
+    photo='https://upload.wikimedia.org/wikipedia/commons/2/29/Disco_ball4.jpg',
+    animal=tiger1, user_id=1)
 
 session.add(tiger1_toy1)
 session.commit()
@@ -136,7 +130,7 @@ session.commit()
 # Toys for Kwan the crocodile
 crocodile1 = Animal(name='Kwan', age=6, species='Crocodile',
     photo='''
-    https://s-media-cache-ak0.pinimg.com/736x/8d/92/2e/8d922e642ce725024ec741bff8bc7175--darwin-australia-saltwater-crocodile.jpg
+    https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Nile_crocodile_head.jpg/260px-Nile_crocodile_head.jpg
     ''', user_id=1)
 
 session.add(crocodile1)
@@ -146,7 +140,7 @@ crocodile1_toy1 = Toy(name='Branches', toy_type='Environmental',
     description='''The crocs love to hide in the branches.
     Maybe they like the privacy?''',
     photo='''
-    http://cdn3.volusion.com/uakvg.mdecs/v/vspfiles/photos/BBNR18-2.jpg?1420709474
+    http://pre04.deviantart.net/bc62/th/pre/i/2015/138/2/2/old_branch_png___by_welshdragonstocknart-d8tt7rf.png
     ''', animal=crocodile1, user_id=1)
 
 session.add(crocodile1_toy1)
@@ -155,8 +149,9 @@ session.commit()
 
 # Toys for Sam the eagle
 eagle1 = Animal(name='Sam', age=4, species='Bald Eagle',
-    photo='https://identify.whatbird.com/img/4/48397/image.aspx?x=322',
-    user_id=1)
+    photo='''
+    http://naturemappingfoundation.org/natmap/photos/birds/bald_eagle_02tk.jpg
+    ''', user_id=1)
 
 session.add(eagle1)
 session.commit()
@@ -165,7 +160,7 @@ eagle1_toy1 = Toy(name='Straw Pile', toy_type='Environmental',
     description='''The eagles love to carry this into their favorite corner
     of the exhibit. Seems like they try to build a nest with it.''',
     photo='''
-    https://blenderartists.org/forum/attachment.php?attachmentid=218183&d=1361347213
+    http://media.istockphoto.com/photos/straw-pile-on-blue-picture-id184292466?k=6&m=184292466&s=612x612&w=0&h=tQL272NaWHFSK-KGQo7lHBv7-us7UZ2zQEqY7xRYNuo=
     ''', animal=eagle1, user_id=1)
 
 session.add(eagle1_toy1)
